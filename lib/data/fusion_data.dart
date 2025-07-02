faf5deef83220e529bce1e4dac61ace35cea5463
schnnjuan <schnnjuan@users.noreply.github.com>
2025-07-01 22:32:20 -0300

    refactor: Adjust FusionRule to match new JSON structure

--- lib/data/fusion_data.dart
+++ lib/data/fusion_data.dart
@@ -2,12 +2,12 @@ class FusionRule {
   final List<String> inputs;
   final String output;
 
-  const FusionRule({required this.inputs, required this.output});
+  FusionRule({required this.inputs, required this.output});
 
   factory FusionRule.fromJson(Map<String, dynamic> json) {
     return FusionRule(
       inputs: List<String>.from(json['inputs']),
-      output: json['output'],
+      output: json['result'],
     );
   }
 }
