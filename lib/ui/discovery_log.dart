5afd89065e2302249424a969b5a88e536cb081d3
schnnjuan <schnnjuan@users.noreply.github.com>
2025-07-01 22:25:37 -0300

    feat: Add DiscoveryLog UI component

--- lib/ui/discovery_log.dart
+++ lib/ui/discovery_log.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:infinitylab/data/fusion_manager.dart';
+import 'package:infinitylab/ui/theme.dart';
 
 class DiscoveryLog extends StatelessWidget {
   const DiscoveryLog({super.key});
@@ -10,25 +11,39 @@ class DiscoveryLog extends StatelessWidget {
     return Consumer<FusionManager>(
       builder: (context, fusionManager, child) {
         final discoveredElements = fusionManager.discoveredElements;
-        return Align(
-          alignment: Alignment.topRight,
-          child: Container(
-            width: 200,
-            height: 300,
-            color: Colors.black.withOpacity(0.5),
-            child: ListView.builder(
-              itemCount: discoveredElements.length,
-              itemBuilder: (context, index) {
-                final element = discoveredElements[index];
-                return Padding(
-                  padding: const EdgeInsets.all(4.0),
-                  child: Text(
-                    '${element.emoji} ${element.name}',
-                    style: TextStyle(color: Colors.white, fontSize: 16),
-                  ),
-                );
-              },
-            ),
+        return Container(
+          width: 160,
+          margin: const EdgeInsets.only(right: 16.0),
+          padding: const EdgeInsets.all(12.0),
+          decoration: const BoxDecoration(
+            color: AppColors.panelBackground,
+            border: Border(right: BorderSide(color: AppColors.borderColor, width: 1.0)),
+            borderRadius: BorderRadius.all(Radius.circular(8.0)),
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Discovery Log',
+                style: AppTextStyles.uppercaseTitle,
+              ),
+              const SizedBox(height: 8.0),
+              Expanded(
+                child: ListView.builder(
+                  itemCount: discoveredElements.length,
+                  itemBuilder: (context, index) {
+                    final element = discoveredElements[index];
+                    return Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 2.0),
+                      child: Text(
+                        '${element.emoji} ${element.name}',
+                        style: AppTextStyles.logEntry,
+                      ),
+                    );
+                  },
+                ),
+              ),
+            ],
           ),
         );
       },
