50a9690331f9bdd5ad32ab90b64cd963d123c5d3
schnnjuan <schnnjuan@users.noreply.github.com>
2025-07-01 22:24:15 -0300

    refactor: Implement vertical inventory with Draggable elements

--- lib/components/inventory_ui.dart
+++ lib/components/inventory_ui.dart
@@ -1,14 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:infinitylab/data/fusion_manager.dart';
-import 'package:infinitylab/components/element_component.dart';
-import 'package:infinitylab/game/infinity_lab_game.dart';
-import 'package:flame/components.dart';
+import 'package:infinitylab/data/element_model.dart';
 
 class InventoryUI extends StatelessWidget {
-  final InfinityLabGame game;
-
-  const InventoryUI({super.key, required this.game});
+  const InventoryUI({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -16,25 +12,50 @@ class InventoryUI extends StatelessWidget {
       builder: (context, fusionManager, child) {
         final discoveredElements = fusionManager.discoveredElements;
         return Align(
-          alignment: Alignment.bottomCenter,
+          alignment: Alignment.centerRight,
           child: Container(
-            height: 100,
+            width: 100, // Width of the inventory column
             color: Colors.grey[800],
             child: ListView.builder(
-              scrollDirection: Axis.horizontal,
+              scrollDirection: Axis.vertical,
               itemCount: discoveredElements.length,
               itemBuilder: (context, index) {
                 final element = discoveredElements[index];
                 return Padding(
                   padding: const EdgeInsets.all(8.0),
-                  child: GestureDetector(
-                    onTap: () {
-                      // Add the element to the game world at a default position
-                      game.addElementAtPosition(
-                        ElementComponent(element: element),
-                        Vector2(game.size.x / 2, game.size.y / 2), // Center of the game world
-                      );
-                    },
+                  child: Draggable<ElementModel>(
+                    data: element,
+                    feedback: Material(
+                      color: Colors.transparent,
+                      child: Container(
+                        width: 50,
+                        height: 50,
+                        decoration: BoxDecoration(
+                          color: Colors.blueGrey[700],
+                          borderRadius: BorderRadius.circular(8),
+                        ),
+                        child: Center(
+                          child: Text(
+                            element.emoji,
+                            style: const TextStyle(fontSize: 30),
+                          ),
+                        ),
+                      ),
+                    ),
+                    childWhenDragging: Container(
+                      width: 50,
+                      height: 50,
+                      decoration: BoxDecoration(
+                        color: Colors.blueGrey[700]!.withOpacity(0.5),
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                      child: Center(
+                        child: Text(
+                          element.emoji,
+                          style: const TextStyle(fontSize: 30),
+                        ),
+                      ),
+                    ),
                     child: Container(
                       width: 50,
                       height: 50,
@@ -45,7 +66,7 @@ class InventoryUI extends StatelessWidget {
                       child: Center(
                         child: Text(
                           element.emoji,
-                          style: TextStyle(fontSize: 30),
+                          style: const TextStyle(fontSize: 30),
                         ),
                       ),
                     ),
